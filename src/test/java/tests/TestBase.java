fedc9b3307a00d8ff4754c41436ea932908a7308
cristal20777 <cristal20777@yandex.ru>
2023-04-13 13:26:34 +0300

    Задание 17 - 2

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,11 +1,14 @@
-package ru.stqa.pft.appmanager;
+package tests;
 
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.appmanager.ApplicationManager;
 
 
 public class TestBase {
+
+
 
   protected static final ApplicationManager
           app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
