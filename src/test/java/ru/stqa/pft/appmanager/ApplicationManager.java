6cb02b9ea966bbdb97cc7aae226e4a551b961c1b
cristal20777 <cristal20777@yandex.ru>
2023-04-13 15:16:11 +0300

    8.4

--- src/test/java/ru/stqa/pft/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/appmanager/ApplicationManager.java
@@ -14,9 +14,9 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   private final Properties properties;
-  public WebDriver wd;
-
+  private WebDriver wd;
   private String browser;
+  private RegistrationHelper registrationHelper;
 
 
   public ApplicationManager(String browser) {
@@ -28,20 +28,42 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-    if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    }
-    wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseURL"));
-
-
   }
 
   public void stop() {
-    wd.quit();
+
+    if (wd != null) {
+      wd.quit();
+    }
+  }
+
+  public HttpSession newSession() {
+    return new HttpSession(this);
+
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null) {
+      if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseURL"));
+    }
+    return wd;
   }
 
 }
