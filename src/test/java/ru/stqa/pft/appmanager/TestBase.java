23003585ad42848fffe5c37e1bad8cf73e3847fa
cristal20777 <cristal20777@yandex.ru>
2023-04-13 13:20:36 +0300

    Initial commit

---
+++ src/test/java/ru/stqa/pft/appmanager/TestBase.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.appmanager;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+
+public class TestBase {
+
+  protected static final ApplicationManager
+          app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite
+  public void tearDown() throws Exception {
+    app.stop();
+
+  }
+  
+}
